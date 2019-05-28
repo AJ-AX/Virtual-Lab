fb4edd1cdbaee7f3378d65ed583e6ea205372ca2
AJ-AX <asathyagal18@simons-rock.edu>
2019-05-28 13:32:27 -0400

    Backend Auth

--- server/index.js
+++ server/index.js
@@ -9,14 +9,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const passport = require('passport');
+const User = require('./models/Users');
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Connect to DB
 mongoose.connect("mongodb://127.0.0.1:27017/srlab", {newUrlParser: true});
 const connection = mongoose.connection;
-
 connection.once('open', () => console.log("Established connection to database."));
+
+// Sessions
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Routing
 
 api.route("/login").post(function(req, res){
     console.log("Recieved LOGIN request.");
@@ -26,6 +40,24 @@ api.route("/login").post(function(req, res){
 api.route("/signup").post(function(req, res){
     console.log("Recieved SIGNUP request.");
     //TODO: Implement auth using passportjs
+});
+
+api.route("/register").post(function(req, res){
+    let password = req.body.password;
+    let confirmPwd = req.body.confirmPwd;
+    if(password == confirmPwd){
+        let newUser = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: password
+        });
+        User.createUser(newUser, (err, user) => {
+            if(err){throw err;}
+            res.send(user).end();
+        })
+    }else{
+        req.status(500).send('{errors: "passwords don\t match."}');
+    }
 });
 
 app.use('/api', api);
---
+++ server/models/Users.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+let UserSchema = mongoose.Schema({
+    username: {
+      type: String,
+      index:true
+    },
+    password: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+  });
+
+
+let User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+module.exports.getByUsername = (username, callback) => {
+    let query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.getById = (id, callback) => {
+    User.findByID(id, callback);
+}
+
+module.exports.comparePassword = function(inputPassword, hash, callback){
+    bcrypt.compare(inputPassword, hash, function(err, isMatch) {
+      if(err){ throw err; }
+      callback(null, isMatch);
+    });
+  }
